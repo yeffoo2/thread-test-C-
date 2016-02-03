7e3c5e5b48a12a00499b075b206d57131cea1e5a
yeffoo <jeff130@gmail.com>
2016-02-03 11:51:18 +0100

    Uruchamianie wątku z przycisku

--- thread_test/thread_test/Form1.Designer.cs
+++ thread_test/thread_test/Form1.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 319);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Test z użyciem wątków";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- thread_test/thread_test/Form1.cs
+++ thread_test/thread_test/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace thread_test
 {
     public partial class Form1 : Form
     {
+        private Thread watek;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void start()
+        {
+            watek = new Thread(funkcja);
+            watek.Start();
+            //watek.Start();
+        }
+
+        private void funkcja()
+        {
+            ulong zmienna = 0;
+
+            for (ulong i = 0; i < 1000; i++)
+            {
+                for (ulong j = 0; j < 1000000; j++)
+                {
+                    zmienna += j;
+                }
+            }
+            textBox1.Invoke(new Action(delegate ()
+            {
+                textBox1.Text = zmienna.ToString();
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start();
+            MessageBox.Show("Liczę w tle");           
         }
     }
 }
